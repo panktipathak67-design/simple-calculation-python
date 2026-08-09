b659d2f157d9c8b58f5f499ee3adeeae1873b0ef
panktipathak67-design <panktipathak67@gmail.com>
2026-08-09 13:27:47 +0530

    Refactor calculator to use a loop for operations

--- simple_calculation.py
+++ simple_calculation.py
@@ -4,27 +4,32 @@ print("\n--- Simple Calculator ---\n")
 a = int(input("Enter the number for a: "))
 b = int(input("Enter the number for b: "))
 
-print("\n----- OPERATIONS -----")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-print("5. Exit")
-
-choice = int(input("Enter the number for choice: "))
-
-match choice:
+while True:
+  print("\n----- OPERATIONS -----")
+  print("1. Addition")
+  print("2. Subtraction")
+  print("3. Multiplication")
+  print("4. Division")
+  print("5. Exit")
+  
+  choice = int(input("Enter the number for choice: "))
+  
+  match choice:
+    case 5:
+      print("\nThanks for using Simple Calculator! \n")
+      break
+    
     case 1:
-        c = a + b
-        print("Addition is:", c)
+      c = a + b
+      print("Addition is:", c)
 
     case 2:
-        c = a - b
-        print("Subtraction is:", c)
+      c = a - b
+      print("Subtraction is:", c)
 
     case 3:
-        c = a * b
-        print("Multiplication is:", c)
+      c = a * b
+      print("Multiplication is:", c)
 
     case 4:
         if b == 0:
@@ -32,11 +37,6 @@ match choice:
         else:
             c = a / b
             print("Division is:", c)
-            
-    case 5:
-      print("Thanks for using Simple Calculator! ")
 
     case _:
         print("Invalid choice")
-
-
