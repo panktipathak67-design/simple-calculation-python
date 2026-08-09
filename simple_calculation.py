c79124f537777f31f9dd6ed728884dcf8db7c73a
panktipathak67-design <panktipathak67@gmail.com>
2026-08-09 12:54:28 +0530

    Update menu and add exit option in calculator

--- simple_calculation.py
+++ simple_calculation.py
@@ -3,11 +3,12 @@
 a = int(input("Enter the number for a: "))
 b = int(input("Enter the number for b: "))
 
-print("\n----- MENU -----")
+print("\n----- OPERATIONS -----")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+print("5. Exit")
 
 choice = int(input("Enter the number for choice: "))
 
@@ -30,6 +31,11 @@ match choice:
         else:
             c = a / b
             print("Division is:", c)
+            
+    case 5:
+      print("Thanks for using Simple Calculator! ")
 
     case _:
         print("Invalid choice")
+
+
